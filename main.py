682389fceab8e0a3706eef39744364df55881a5c
Jatin <jatin.dhar27@gmail.com>
2024-05-04 11:36:14 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
-@app.get("/")
+@app.api_route("/", methods=['GET', 'POST'])
 def root():
     return {"message":"Hello World"}
 
-@app.get("/posts")
+@app.api_route("/posts", methods=['GET', 'POST'])
 def get_posts():
     return {"date": "This is your posts"}
+
+@app.api_route('/createposts', methods=['GET', 'POST'])
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
